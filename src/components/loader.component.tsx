6e66a74f7927f6d336b823153abf6ea649cde95c
Johnny Ekparam <johnnye4u2c@gmail.com>
2023-06-15 00:11:21 +0100

    added a loader component

--- src/components/loader.component.tsx
+++ src/components/loader.component.tsx
@@ -3,9 +3,7 @@ import React, { useState, useEffect } from "react";
 const Loader: React.FC = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="animate-spin rounded-full h-10 w-10 md:h-16 md:w-16 border-t-4 border-b-4 border-accent">
-        {" "}
-      </div>
+      <div className="animate-spin rounded-full h-10 w-10 md:h-16 md:w-16 border-t-4 border-b-4 border-accent"></div>
       <h1 className="py-3 text-4xl md:text-6xl  animate-pulse">
         {" "}
         <span className="text-accent">@</span>cyber_jev
@@ -25,11 +23,11 @@ const LoaderComponent: React.FC<LoaderComponentProps> = ({ children }) => {
     // Simulating a delay to demonstrate the loader
     setTimeout(() => {
       setIsLoading(false);
-    }, 3000);
+    }, 2500);
   }, []);
 
   return (
-    <div className="bg-bg-body m-auto transition-all transform ease-in-out">
+    <div className="bg-bg-body m-auto transition-opacity transition-transform duration-500 ease-in-out">
       {isLoading ? <Loader /> : children}
     </div>
   );
