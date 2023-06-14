5b390c3d897ec380434fce68026adf4f90b79b20
Johnny Ekparam <johnnye4u2c@gmail.com>
2023-06-14 23:57:21 +0100

    added a loader component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from "react";
 import "./App.css";
 import { stackItems, projects } from "./utils/data.utils";
+import LoaderComponent from "./components/loader.component";
 import Hero from "./components/hero.component";
 import TeckStack from "./components/tech-stack.component";
 import Projects from "./components/projects.component";
 import Footer from "./components/footer.component";
-import { Line } from "./components/btn.component";
 
 function App() {
   return (
-    <div className="bg-bg-body m-auto">
-      <header className="App-header">
-        <Hero />
-      </header>
-      <main>
-        <TeckStack items={stackItems} />
-        <Projects works={projects} />
-      </main>
-      <Footer />
-    </div>
+    <LoaderComponent>
+      <div className="bg-bg-body m-auto">
+        <header className="App-header">
+          <Hero />
+        </header>
+        <main>
+          <TeckStack items={stackItems} />
+          <Projects works={projects} />
+        </main>
+        <Footer />
+      </div>
+    </LoaderComponent>
   );
 }
 
---
+++ src/components/loader.component.tsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+
+const Loader: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="animate-spin rounded-full h-10 w-10 md:h-16 md:w-16 border-t-4 border-b-4 border-accent">
+        {" "}
+      </div>
+      <h1 className="py-3 text-4xl md:text-6xl  animate-pulse">
+        {" "}
+        <span className="text-accent">@</span>cyber_jev
+      </h1>
+    </div>
+  );
+};
+
+interface LoaderComponentProps {
+  children: React.ReactNode;
+}
+
+const LoaderComponent: React.FC<LoaderComponentProps> = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulating a delay to demonstrate the loader
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
+
+  return (
+    <div className="bg-bg-body m-auto transition-all transform ease-in-out">
+      {isLoading ? <Loader /> : children}
+    </div>
+  );
+};
+
+export default LoaderComponent;
