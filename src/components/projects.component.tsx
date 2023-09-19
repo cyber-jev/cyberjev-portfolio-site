b21464c86deeb9dba6ef75936bc560a9d262ceb2
Johnny Ekparam <johnnye4u2c@gmail.com>
2023-09-19 13:10:14 +0100

    added scroll animation to projects component

--- src/components/projects.component.tsx
+++ src/components/projects.component.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Btn } from "./btn.component";
 
 type ProjectsType = {
@@ -14,11 +14,36 @@ type ProjectProps = {
   works: ProjectsType[];
 };
 
-//? single project
 const Project: React.FC<{ work: ProjectsType }> = ({ work }) => {
+  // ?scroll animation starts
+  useEffect(() => {
+    const handleScroll = () => {
+      const trigger = (window.innerHeight / 5) * 4;
+      const boxes = document.querySelectorAll(".animate-on-scroll");
+
+      boxes.forEach((box) => {
+        const boxTop = box.getBoundingClientRect().top;
+
+        if (boxTop < trigger) {
+          box.classList.add("show");
+        } else {
+          box.classList.remove("show");
+        }
+      });
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  //?scroll animation ends
+
   const { id, title, image, techUsed, projectLink, codeLink } = work;
   return (
-    <div className="py-4 relative group overflow-hidden ">
+    <div className="py-4 relative group overflow-hidden animate-on-scroll">
       <div className="overflow-hidden ">
         <img
           src={image}
@@ -29,7 +54,9 @@ const Project: React.FC<{ work: ProjectsType }> = ({ work }) => {
       <h3 className="text-xl font-bold py-2">{title}</h3>
       <div className="flex gap-2">
         {techUsed.map((tech) => (
-          <p className="text-text2">{tech}</p>
+          <p className="text-text2" key={tech}>
+            {tech}
+          </p>
         ))}
       </div>
       <div className="lg:h-[100%] lg:w-full lg:hidden lg:absolute lg:top-0 lg:group-hover:block lg:hover:backdrop-filter lg:hover:backdrop-blur ">
@@ -42,15 +69,14 @@ const Project: React.FC<{ work: ProjectsType }> = ({ work }) => {
   );
 };
 
-// ? all projects
 const Projects: React.FC<ProjectProps> = ({ works }) => {
   return (
-    <section className="p-2.5 max-w-[1200px] m-auto">
+    <section className="p-2.5 max-w-[1200px] m-auto ">
       <div className="flex justify-between items-center">
         <h2 className="text-2xl font-bold">Projects</h2>
         <Btn title="CONTACT ME" url="#contact-me" />
       </div>
-      <div className="py-6 md:grid md:grid-cols-2 gap-4">
+      <div className="py-6 md:grid md:grid-cols-2 gap-4 overflow-hidden">
         {works.map((work) => (
           <Project key={work.id} work={work} />
         ))}
